e06a4930af74a1e16f393cd4a74e73b73aac2ad8
Paul Hodge <paul.hodge.email@gmail.com>
2011-09-29 17:43:57 -0700

    WIP on fixing vectorized funcs

--- src/functions/vectorize_vs.cpp
+++ src/functions/vectorize_vs.cpp
@@ -23,68 +23,28 @@ namespace vectorize_vs_function {
 
     CA_FUNCTION(evaluate)
     {
-#if 0 // FIXME
+        // Fetch function
+        TaggedValue* funcParam = &get_function_attrs(CALLER->function)->parameter;
+        Term* funcTerm = as_ref(funcParam);
+        EvaluateFunc func = get_function_attrs(funcTerm)->evaluate;
+
         EvalContext* context = CONTEXT;
-        Branch* contents = nested_contents(CALLER);
-        TaggedValue input0, input1;
-        copy(INPUT(0), &input0);
-        copy(INPUT(1), &input1);
-        int listLength = input0.numElements();
 
-        Term* input0_placeholder = contents->get(0);
-        Term* input1_placeholder = contents->get(1); 
-        Term* content_output = contents->get(2); 
+        TaggedValue* left = INPUT(0);
+        TaggedValue* right = INPUT(1);
+        int count = left->numElements();
 
-        push_stack_frame(CONTEXT, contents);
+        List* output = set_list(OUTPUT, count);
 
-        // Prepare output
-        List output;
-        output.resize(listLength);
+        TaggedValue* pointers[3];
 
-        // Copy right input once
-        swap(&input1, get_local(context, 0, input1_placeholder));
+        pointers[1] = right;
 
-        // Evaluate vectorized call, once for each input
-        for (int i=0; i < listLength; i++) {
-            // Copy left into placeholder
-            swap(input0.getIndex(i), get_local(context, 0, input0_placeholder));
-
-            evaluate_single_term_with_bytecode(CONTEXT, content_output);
-
-            // Save output
-            swap(get_local(context, 0, content_output), output[i]);
+        for (int i=0; i < count; i++) {
+            pointers[0] = left->getIndex(i);
+            pointers[2] = output->get(i);
+            func(context, 3, pointers);
         }
-
-        pop_stack_frame(CONTEXT);
-
-        swap(&output, OUTPUT);
-#endif
-    }
-
-    void post_input_change(Term* term)
-    {
-        // Update generated code
-        Branch* contents = nested_contents(term);
-        clear_branch(contents);
-
-        TaggedValue* funcParam = &get_function_attrs(term->function)->parameter;
-        if (funcParam == NULL || !is_ref(funcParam))
-            return;
-
-        Term* func = as_ref(funcParam);
-        Term* left = term->input(0);
-        Term* right = term->input(1);
-
-        if (func == NULL || left == NULL || right == NULL)
-            return;
-
-        Term* leftPlaceholder = apply(contents, INPUT_PLACEHOLDER_FUNC, TermList());
-        change_declared_type(leftPlaceholder, infer_type_of_get_index(left));
-
-        Term* rightPlaceholder = apply(contents, INPUT_PLACEHOLDER_FUNC, TermList());
-        change_declared_type(rightPlaceholder, right->type);
-
-        apply(contents, func, TermList(leftPlaceholder, rightPlaceholder));
     }
 
     void setup(Branch* kernel)
@@ -92,7 +52,6 @@ namespace vectorize_vs_function {
         Term* func = import_function(kernel, evaluate,
                 "vectorize_vs(List,any) -> List");
         get_function_attrs(func)->specializeType = specializeType;
-        get_function_attrs(func)->postInputChange = post_input_change;
     }
 }
 } // namespace circa
